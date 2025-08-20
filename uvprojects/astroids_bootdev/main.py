c2799aee57fd31a7b79cf542dfc5291908708373
e-s2055 <elizabeth.b.s@protonmail.com>
2025-08-20 18:47:00 +0200

    Now opens black game window

--- uvprojects/astroids_bootdev/main.py
+++ uvprojects/astroids_bootdev/main.py
@@ -1,9 +1,15 @@
 import pygame, constants
+from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        screen.fill((0,0,0), rect=None, special_flags=0)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
