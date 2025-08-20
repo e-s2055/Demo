91f030c463d033c80b8b9b1150d3f2295b08dace
e-s2055 <elizabeth.b.s@protonmail.com>
2025-08-20 18:48:03 +0200

    Now closable via x of game window instead of terminal

--- uvprojects/astroids_bootdev/main.py
+++ uvprojects/astroids_bootdev/main.py
@@ -8,6 +8,9 @@ def main():
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill((0,0,0), rect=None, special_flags=0)
         pygame.display.flip()
 
