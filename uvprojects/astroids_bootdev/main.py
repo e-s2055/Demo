ae6517b08ee2603a11d516d1e092f69d533b5ea2
e-s2055 <elizabeth.b.s@protonmail.com>
2025-08-21 18:38:30 +0200

    Added player.py, renders the player on the screen

--- uvprojects/astroids_bootdev/main.py
+++ uvprojects/astroids_bootdev/main.py
@@ -1,4 +1,7 @@
-import pygame, constants
+import pygame
+import constants
+import circleshape 
+import player
 from constants import *
 
 def main():
@@ -9,11 +12,13 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    player1 = player.Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect=None, special_flags=0)        
+        player1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
