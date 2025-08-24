4d7df81cd74d2c634c3a5efc042efd22b5290a31
e-s2055 <elizabeth.b.s@protonmail.com>
2025-08-24 13:58:41 +0200

    Refactored: Added pygame Groups for organization (updatable, drawable), applied in main()

--- uvprojects/astroids_bootdev/main.py
+++ uvprojects/astroids_bootdev/main.py
@@ -12,14 +12,18 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = (updatable, drawable)
     player1 = player.Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0), rect=None, special_flags=0)        
-        player1.draw(screen)
-        player1.update(dt)
+        for group_object in drawable:
+            group_object.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
