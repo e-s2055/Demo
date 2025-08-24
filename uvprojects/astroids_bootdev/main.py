cb21cbca7a330b1ba265e00bd2923d81d06bc87c
e-s2055 <elizabeth.b.s@protonmail.com>
2025-08-24 17:48:44 +0200

    Feature: Added Asteroid object, Added Asteroid Field with randomized spawning

--- uvprojects/astroids_bootdev/main.py
+++ uvprojects/astroids_bootdev/main.py
@@ -2,6 +2,8 @@ import pygame
 import constants
 import circleshape 
 import player
+import asteroidfield
+import asteroid
 from constants import *
 
 def main():
@@ -14,8 +16,12 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     player.Player.containers = (updatable, drawable)
+    asteroidfield.AsteroidField.containers = (updatable, )
+    asteroid.Asteroid.containers = (asteroids, updatable, drawable)
     player1 = player.Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroid_field = asteroidfield.AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
