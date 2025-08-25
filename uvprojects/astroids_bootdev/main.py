35b454b6ad14bba8f8e19a8185f4041a123aec16
e-s2055 <elizabeth.b.s@protonmail.com>
2025-08-25 21:51:01 +0200

    Add: CW - CSS, Asteroids now has collision player VS asteroids

--- uvprojects/astroids_bootdev/main.py
+++ uvprojects/astroids_bootdev/main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 import constants
 import circleshape 
@@ -5,6 +6,7 @@ import player
 import asteroidfield
 import asteroid
 from constants import *
+
 
 def main():
     pygame.init()
@@ -30,6 +32,9 @@ def main():
         for group_object in drawable:
             group_object.draw(screen)
         updatable.update(dt)
+        for one_asteroid in asteroids:
+            if player1.collision(one_asteroid) is True:
+                raise SystemExit (sys.exit("Game Over!"))
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
